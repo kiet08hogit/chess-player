a7d7cea6da92ab935172f12a78308a1ad561925b
nho <nho@uic.edu>
2026-04-21 23:02:56 -0500

    update UI, add room joining, rule section

---
+++ Client/src/main/java/GuiClient.java
@@ -0,0 +1,244 @@
+import java.util.HashMap;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.animation.FadeTransition;
+import javafx.animation.Transition;
+
+public class GuiClient extends Application {
+
+    Client clientConnection;
+    Stage primaryStage;
+    
+    // View References
+    LoginView loginView;
+    HomeView homeView;
+    GameView gameView;
+    
+    // Persistent Scenes
+    Scene loginScene;
+    Scene homeScene;
+    Scene gameScene;
+    Scene waitingScene;
+    
+    // Global State
+    private Label waitingTitleLabel;
+    private Label waitingSubLabel;
+    String loggedInUser = "";
+    boolean isP1;
+    boolean isSpectatorMode = false;
+    int selectedX = -1;
+    int selectedY = -1;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        
+        clientConnection = new Client(data -> {
+            Platform.runLater(() -> handleServerMessage((Message) data));
+        });
+        clientConnection.start();
+
+        // Initialize views
+        loginView = new LoginView(this);
+        homeView = new HomeView(this);
+        gameView = new GameView(this);
+
+        // Create persistent scenes
+        loginScene = new Scene(loginView.getRoot(), 500, 450);
+        homeScene = new Scene(homeView.getRoot(), 1150, 750);
+        gameScene = new Scene(gameView.getRoot(), 1150, 750);
+        waitingScene = createWaitingGui();
+
+        primaryStage.setOnCloseRequest(e -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
+        primaryStage.setScene(loginScene);
+        primaryStage.setTitle("Checkers Login");
+        primaryStage.show();
+    }
+    
+    public void onLoginAttempt(String user) {
+        this.loggedInUser = user;
+        Message m = new Message(Message.Action.LOGIN);
+        m.username = user;
+        clientConnection.send(m);
+    }
+
+    public void onFindMatch() {
+        clientConnection.send(new Message(Message.Action.FIND_MATCH));
+        waitingTitleLabel.setText("Searching for opponent...");
+        waitingSubLabel.setText("Please wait while we connect you to a game...");
+        primaryStage.setScene(waitingScene);
+        primaryStage.setTitle("Searching for Match...");
+    }
+
+    public void onCreateRoom() {
+        clientConnection.send(new Message(Message.Action.CREATE_ROOM));
+        waitingTitleLabel.setText("Creating room...");
+        waitingSubLabel.setText("Please wait...");
+        primaryStage.setScene(waitingScene);
+        primaryStage.setTitle("Creating Room...");
+    }
+
+    public void onJoinFriend(String roomId) {
+        Message m = new Message(Message.Action.JOIN_ROOM);
+        m.roomId = roomId;
+        clientConnection.send(m);
+        primaryStage.setScene(waitingScene);
+        primaryStage.setTitle("Joining Room #" + roomId + "...");
+    }
+
+    public void onWatchMatch(String roomId) {
+        Message m = new Message(Message.Action.WATCH_MATCH);
+        m.roomId = roomId;
+        clientConnection.send(m);
+    }
+
+    private void handleServerMessage(Message data) {
+        if (data.action == Message.Action.LOGIN_SUCCESS) {
+            homeView.setUserLabel(loggedInUser);
+            primaryStage.setScene(homeScene);
+            primaryStage.setTitle("Checkers - Home");
+        } 
+        else if (data.action == Message.Action.CREATE_ROOM) {
+            waitingTitleLabel.setText("Room ID: " + data.roomId);
+            waitingSubLabel.setText("Tell your friend to enter this code to join you!");
+        }
+        else if (data.action == Message.Action.LOGIN_FAIL) {
+            new Alert(AlertType.ERROR, "Login Failed: " + data.content).show();
+        } 
+        else if (data.action == Message.Action.MATCH_FOUND || data.action == Message.Action.GAME_STATE_UPDATE) {
+            if (primaryStage.getScene() != gameScene) {
+                primaryStage.setScene(gameScene);
+                gameView.clearChat();
+                gameView.resetUI();
+            }
+            
+            if (data.action == Message.Action.MATCH_FOUND) {
+                this.isP1 = data.isPlayer1;
+                this.isSpectatorMode = false;
+                primaryStage.setTitle("Checkers Room #" + data.roomId);
+                gameView.addChatMessage("System: Joined match " + data.roomId);
+            }
+            
+            if (data.isSpectator) {
+                this.isSpectatorMode = true;
+                this.isP1 = true; 
+                primaryStage.setTitle("Spectating Room #" + data.roomId);
+            }
+            
+            gameView.updateState(data);
+        } 
+        else if (data.action == Message.Action.CHAT_MESSAGE) {
+            gameView.addChatMessage(data.username + ": " + data.content);
+        }
+        else if (data.action == Message.Action.GLOBAL_CHAT) {
+            homeView.addGlobalChatMessage(data.username + ": " + data.content);
+        }
+        else if (data.action == Message.Action.UPDATE_PLAYER_LIST) {
+            homeView.updatePlayerList(data);
+        }
+        else if (data.action == Message.Action.GAME_OVER) {
+            gameView.updateState(data);
+            gameView.showGameOver(data.content);
+        }
+        else if (data.action == Message.Action.OPPONENT_LEFT) {
+            new Alert(AlertType.WARNING, "Match ended. Returning to Home.").show();
+            primaryStage.setScene(homeScene);
+        }
+        else if (data.action == Message.Action.ERROR) {
+            new Alert(AlertType.WARNING, data.content).show();
+            if (data.content.contains("Match ended") || data.content.contains("Room not found")) {
+                primaryStage.setScene(homeScene);
+            }
+        }
+    }
+
+    public void handleTileClick(int x, int y, int piece) {
+        boolean ownPiece = false;
+        if (isP1 && (piece == 1 || piece == 3)) ownPiece = true;
+        if (!isP1 && (piece == 2 || piece == 4)) ownPiece = true;
+
+        if (selectedX == -1) {
+            if (ownPiece) {
+                selectedX = x;
+                selectedY = y;
+                gameView.drawBoard();
+            }
+        } else {
+            if (x == selectedX && y == selectedY) {
+                selectedX = -1;
+                selectedY = -1;
+                gameView.drawBoard();
+            } else if (ownPiece) {
+                selectedX = x;
+                selectedY = y;
+                gameView.drawBoard();
+            } else {
+                Message m = new Message(Message.Action.MOVE);
+                m.startX = selectedX;
+                m.startY = selectedY;
+                m.endX = x;
+                m.endY = y;
+                clientConnection.send(m);
+                selectedX = -1;
+                selectedY = -1;
+            }
+        }
+    }
+    
+    public void returnToHome() {
+        clientConnection.send(new Message(Message.Action.PLAY_AGAIN));
+        primaryStage.setScene(homeScene);
+    }
+
+    private Scene createWaitingGui() {
+        VBox root = new VBox(30);
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(50));
+        try { root.getStylesheets().add(getClass().getResource("/style.css").toExternalForm()); } catch (Exception e) {}
+        waitingTitleLabel = new Label("Searching for opponent...");
+        waitingTitleLabel.getStyleClass().add("searching-label");
+        
+        Circle pulse = new Circle(30, Color.web("#DC2626"));
+        FadeTransition ft = new FadeTransition(Duration.seconds(1), pulse);
+        ft.setFromValue(1.0);
+        ft.setToValue(0.3);
+        ft.setCycleCount(Transition.INDEFINITE);
+        ft.setAutoReverse(true);
+        ft.play();
+        
+        waitingSubLabel = new Label("Please wait while we connect you to a game...");
+        waitingSubLabel.setStyle("-fx-text-fill: #9CA3AF; -fx-font-size: 14px;");
+        
+        Button backHomeBtn = new Button("BACK HOME");
+        backHomeBtn.getStyleClass().add("menu-button");
+        backHomeBtn.setMinWidth(200);
+        backHomeBtn.setOnAction(e -> {
+            clientConnection.send(new Message(Message.Action.CANCEL_WAITING));
+            primaryStage.setScene(homeScene);
+            primaryStage.setTitle("Checkers - Home");
+        });
+
+        root.getChildren().addAll(waitingTitleLabel, pulse, waitingSubLabel, backHomeBtn);
+        return new Scene(root, 500, 450);
+    }
+}
---
+++ Client/src/main/java/HomeView.java
@@ -0,0 +1,273 @@
+import java.util.Optional;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class HomeView {
+    private BorderPane root;
+    private ListView<String> onlinePlayersList;
+    private ListView<String> globalChatList;
+    private TextField globalChatField;
+    private Label userLabel;
+    private GuiClient mainApp;
+
+    public HomeView(GuiClient mainApp) {
+        this.mainApp = mainApp;
+        createGui();
+    }
+
+    private void createGui() {
+        root = new BorderPane();
+        try {
+            root.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+        } catch (Exception e) {}
+        root.getStyleClass().add("home-root");
+
+        // --- Left Sidebar ---
+        VBox leftSidebar = new VBox();
+        leftSidebar.getStyleClass().add("sidebar");
+        leftSidebar.setMinWidth(220); 
+        
+        Button playRandom = new Button("RANDOM MATCH");
+        Button createRoom = new Button("CREATE ROOM");
+        Button joinRoom = new Button("JOIN ROOM");
+        Button watchMatch = new Button("WATCH MATCH");
+        Button playBot = new Button("PLAY WITH BOT");
+        Button communityBtn = new Button("COMMUNITY");
+        Button readRules = new Button("READ RULE");
+        
+        playRandom.getStyleClass().add("menu-button");
+        createRoom.getStyleClass().add("menu-button");
+        joinRoom.getStyleClass().add("menu-button");
+        watchMatch.getStyleClass().add("menu-button");
+        playBot.getStyleClass().add("menu-button");
+        communityBtn.getStyleClass().add("menu-button");
+        readRules.getStyleClass().add("menu-button");
+        
+        for (Button b : new Button[]{playRandom, createRoom, joinRoom, watchMatch, playBot, communityBtn, readRules}) {
+            b.setMinWidth(200);
+            b.setMaxWidth(Double.MAX_VALUE);
+        }
+
+        playRandom.setOnAction(e -> mainApp.onFindMatch());
+        
+        createRoom.setOnAction(e -> mainApp.onCreateRoom());
+        
+        joinRoom.setOnAction(e -> {
+            TextInputDialog dialog = new TextInputDialog();
+            dialog.setTitle("Join Room");
+            dialog.setHeaderText("Enter Room ID to join your friend");
+            dialog.setContentText("Room ID:");
+            Optional<String> result = dialog.showAndWait();
+            result.ifPresent(roomId -> mainApp.onJoinFriend(roomId));
+        });
+        
+        watchMatch.setOnAction(e -> {
+            TextInputDialog dialog = new TextInputDialog();
+            dialog.setTitle("Watch Match");
+            dialog.setHeaderText("Enter Room ID to spectate");
+            dialog.setContentText("Room ID:");
+            Optional<String> result = dialog.showAndWait();
+            result.ifPresent(roomId -> mainApp.onWatchMatch(roomId));
+        });
+        
+        // --- Center Area (Static Board) ---
+        StackPane centerWrapper = new StackPane();
+        centerWrapper.setPadding(new Insets(20));
+        GridPane homeBoard = createStaticBoard();
+        
+        centerWrapper.layoutBoundsProperty().addListener((obs, oldV, newV) -> {
+            // homeBoard is about 400x400
+            double scale = Math.min(newV.getWidth() / 440.0, newV.getHeight() / 440.0);
+            if (scale > 0 && homeBoard.isVisible()) {
+                homeBoard.setScaleX(scale);
+                homeBoard.setScaleY(scale);
+            }
+        });
+        
+        VBox rulesBox = new VBox(20);
+        rulesBox.setAlignment(Pos.CENTER);
+        rulesBox.setStyle("-fx-background-color: #1E293B; -fx-padding: 30; -fx-background-radius: 10; -fx-border-color: #334155; -fx-border-width: 2;");
+        rulesBox.setMaxSize(400, 400); 
+        
+        Label rulesTitle = new Label("Game Rules");
+        rulesTitle.getStyleClass().add("title-label");
+        
+        Label rulesText = new Label(
+            "1. Red always moves first.\n\n" +
+            "2. Pieces move diagonally forward to an empty square.\n\n" +
+            "3. Jump over an opponent's piece to capture it.\n\n" +
+            "4. Reach the opposite end of the board to become a King.\n\n" +
+            "5. Kings can move and jump both forward and backward.\n\n" +
+            "6. Win by capturing all of your opponent's pieces."
+        );
+        rulesText.setWrapText(true);
+        rulesText.setStyle("-fx-text-fill: #E2E8F0; -fx-font-size: 14px; -fx-line-spacing: 5px;");
+        
+        rulesBox.getChildren().addAll(rulesTitle, rulesText);
+        rulesBox.setVisible(false);
+        
+        VBox globalChatBox = new VBox(15);
+        globalChatBox.setAlignment(Pos.CENTER);
+        globalChatBox.setStyle("-fx-background-color: #1E293B; -fx-padding: 30; -fx-background-radius: 10; -fx-border-color: #334155; -fx-border-width: 2;");
+        globalChatBox.setMaxSize(400, 400); 
+        
+        Label chatTitle = new Label("Community Chat");
+        chatTitle.getStyleClass().add("title-label");
+        
+        globalChatList = new ListView<>();
+        globalChatList.getStyleClass().add("chat-list");
+        globalChatList.setPrefHeight(250);
+        
+        HBox chatInputLine = new HBox(10);
+        chatInputLine.setAlignment(Pos.CENTER);
+        globalChatField = new TextField();
+        globalChatField.setPromptText("Say hello to the community...");
+        globalChatField.setPrefWidth(220);
+        Button sendChatBtn = new Button("SEND");
+        sendChatBtn.setMinWidth(80);
+        
+        sendChatBtn.setOnAction(e -> {
+            if (!globalChatField.getText().isEmpty()) {
+                Message mg = new Message(Message.Action.GLOBAL_CHAT);
+                mg.content = globalChatField.getText();
+                mainApp.clientConnection.send(mg);
+                globalChatField.clear();
+            }
+        });
+        
+        chatInputLine.getChildren().addAll(globalChatField, sendChatBtn);
+        globalChatBox.getChildren().addAll(chatTitle, globalChatList, chatInputLine);
+        globalChatBox.setVisible(false);
+        
+        centerWrapper.getChildren().addAll(homeBoard, rulesBox, globalChatBox);
+        root.setCenter(centerWrapper);
+        
+        communityBtn.setOnAction(e -> {
+            boolean showChat = !globalChatBox.isVisible();
+            globalChatBox.setVisible(showChat);
+            rulesBox.setVisible(false);
+            homeBoard.setVisible(!showChat);
+        });
+
+        readRules.setOnAction(e -> {
+            boolean showRules = !rulesBox.isVisible();
+            rulesBox.setVisible(showRules);
+            globalChatBox.setVisible(false);
+            homeBoard.setVisible(!showRules);
+        });
+
+        leftSidebar.getChildren().addAll(playRandom, createRoom, joinRoom, watchMatch, playBot, communityBtn, readRules);
+        root.setLeft(leftSidebar);
+
+        // --- Right Sidebar ---
+        VBox rightSidebar = new VBox();
+        rightSidebar.getStyleClass().add("player-list");
+        rightSidebar.setMinWidth(220);
+        Label listTitle = new Label("Online Players");
+        listTitle.setStyle("-fx-text-fill: white; -fx-font-weight: bold;");
+        onlinePlayersList = new ListView<>();
+        onlinePlayersList.setMinWidth(200);
+        rightSidebar.getChildren().addAll(listTitle, onlinePlayersList);
+        root.setRight(rightSidebar);
+
+        // --- Bottom Bar ---
+        HBox bottomBar = new HBox();
+        bottomBar.getStyleClass().add("user-info-bar");
+        bottomBar.setAlignment(Pos.CENTER_LEFT);
+        userLabel = new Label("user: ");
+        userLabel.getStyleClass().add("user-label");
+        bottomBar.getChildren().add(userLabel);
+        root.setBottom(bottomBar);
+    }
+
+    private GridPane createStaticBoard() {
+        GridPane board = new GridPane();
+        board.setHgap(0); board.setVgap(0);
+        board.setAlignment(Pos.CENTER);
+        board.setStyle("-fx-background-color: #5C3A21; -fx-background-radius: 8; -fx-border-color: #3B2313; -fx-border-width: 5; -fx-border-radius: 8; -fx-padding: 15;");
+        
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
+                VBox cell = new VBox();
+                cell.setPrefSize(45, 45);
+                cell.setAlignment(Pos.CENTER);
+                
+                if ((x + y) % 2 == 0) {
+                    cell.setStyle("-fx-background-color: #F8CB9C;");
+                } else {
+                    cell.setStyle("-fx-background-color: #D28C45;");
+                }
+                
+                if ((x+y)%2 != 0) {
+                    if (y < 3) {
+                        Circle c = new Circle(18);
+                        c.setStrokeType(javafx.scene.shape.StrokeType.INSIDE);
+                        c.setStrokeWidth(2.5);
+                        c.setFill(Color.web("#1E293B"));
+                        c.setStroke(Color.web("#0F172A"));
+                        
+                        Circle innerObj = new Circle(11);
+                        innerObj.setFill(Color.TRANSPARENT);
+                        innerObj.setStrokeWidth(2);
+                        innerObj.setStroke(Color.web("#334155"));
+                        
+                        cell.getChildren().add(new javafx.scene.layout.StackPane(c, innerObj));
+                    } else if (y > 4) {
+                        Circle c = new Circle(18);
+                        c.setStrokeType(javafx.scene.shape.StrokeType.INSIDE);
+                        c.setStrokeWidth(2.5);
+                        c.setFill(Color.web("#EF4444"));
+                        c.setStroke(Color.web("#B91C1C"));
+                        
+                        Circle innerObj = new Circle(11);
+                        innerObj.setFill(Color.TRANSPARENT);
+                        innerObj.setStrokeWidth(2);
+                        innerObj.setStroke(Color.web("#DC2626"));
+                        
+                        cell.getChildren().add(new javafx.scene.layout.StackPane(c, innerObj));
+                    }
+                }
+                board.add(cell, x, y);
+            }
+        }
+        board.setMinSize(400, 400);
+        board.setMaxSize(400, 400);
+        return board;
+    }
+
+    public void updatePlayerList(Message data) {
+        onlinePlayersList.getItems().clear();
+        if (data.playerStatusMap != null) {
+            data.playerStatusMap.forEach((user, status) -> {
+                onlinePlayersList.getItems().add(user + " (" + status + ")");
+            });
+        }
+    }
+
+    public void setUserLabel(String user) {
+        userLabel.setText("user: " + user);
+    }
+    
+    public void addGlobalChatMessage(String msg) {
+        if (globalChatList != null) {
+            globalChatList.getItems().add(msg);
+        }
+    }
+
+    public Parent getRoot() {
+        return root;
+    }
+}
